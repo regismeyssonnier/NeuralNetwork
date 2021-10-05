16a760c6ef779d94b4827b1d86aef75f02dcc94e
Régis Meyssonnier <67794100+regismeyssonnier@users.noreply.github.com>
2021-10-05 21:11:07 +0200

    Add files via upload

---
+++ nn.py
@@ -0,0 +1,405 @@
+
+
+import random
+import math
+
+
+inputn = []
+size_input = 2500
+hidden = []
+hiddenw = []
+hiddenb = []
+nb_hidden = 2
+size_hidden = 16
+size_hidden2 = 8
+sz_hidden = [size_hidden, size_hidden2]
+size_hidden_weight = [size_input, size_hidden]
+size_hidden_bias = [size_hidden, size_hidden2]
+
+output = []
+outputw = []
+outputb = []
+size_output = 2
+size_output_weight = size_hidden2
+size_output_bias = size_output
+LR = 0.1
+
+TEST = []
+TEST_out = []
+
+
+def test_proc(test, out):
+	
+	TEST.append(test)
+	TEST_out.append(out)
+	
+
+def display_test(test, out):
+
+	print("output :  " + str(out))
+	for i in range(5):
+		d = ''
+		for j in range(5):
+			d += str(test[i*5+j]) + " "
+		print(d)
+	
+
+def display_TEST():
+	for i in range(len(TEST)):
+		display_test(TEST[i], TEST_out[i])
+		print("")
+
+def test2inputn(test):
+	inp = []
+	for t in test:
+		inp.append(t)
+	return inp
+
+
+def display_inputn(inp):
+	print(inp)
+	
+"""
+test = [0, 0, 1, 0, 0,
+	0, 0, 1, 0, 0,
+	1, 1, 1, 1, 1,
+	0, 0, 1, 0, 0,
+	0, 0, 1, 0, 0]
+
+test_proc(test, 1)
+
+test = [0, 0, 1, 0, 0,
+	0, 0, 1, 0, 0,
+	0, 1, 1, 1, 1,
+	0, 0, 1, 0, 0,
+	0, 0, 1, 0, 0]
+
+test_proc(test, 1)
+
+test = [0, 0, 1, 0, 0,
+	0, 0, 1, 0, 0,
+	1, 1, 1, 1, 0,
+	0, 0, 1, 0, 0,
+	0, 0, 1, 0, 0]
+
+test_proc(test, 1)
+
+test = [0, 0, 0, 0, 0,
+	0, 0, 1, 0, 0,
+	1, 1, 1, 1, 1,
+	0, 0, 1, 0, 0,
+	0, 0, 1, 0, 0]
+
+test_proc(test, 1)
+
+test = [0, 0, 1, 0, 0,
+	0, 0, 1, 0, 0,
+	1, 1, 1, 1, 1,
+	0, 0, 1, 0, 0,
+	0, 0, 0, 0, 0]
+
+test_proc(test, 1)
+
+test = [0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0,
+	1, 1, 1, 1, 1,
+	0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0]
+
+test_proc(test, 2)
+
+test = [0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0,
+	0, 1, 1, 1, 1,
+	0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0]
+
+test_proc(test, 2)
+
+test = [0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0,
+	1, 1, 1, 1, 0,
+	0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0]
+
+test_proc(test, 2)
+
+test = [0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0,
+	0, 0.5, 1, 1, 1,
+	0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0]
+
+test_proc(test, 2)
+
+
+test = [0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0,
+	1, 1, 1, 0.5, 0,
+	0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0]
+
+test_proc(test, 2)
+
+test = [0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0,
+	1, 1, 1, 0.5, 0,
+	0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0]
+
+test_proc(test, 2)
+
+test = [0, 0, 0, 0, 0,
+	1, 1, 0, 0, 0,
+	0, 0, 1, 0, 0,
+	0, 0, 0, 1, 1,
+	0, 0, 0, 0, 0]
+
+test_proc(test, 2)
+"""
+#********************************************************************************
+
+def display_hidden(hid):
+	for h in hid:
+		print("size=" + str(len(h)))
+		print(h)
+
+def display_hidden_w(hid):
+	for h in hid:
+		print("size=" + str(len(h)))
+		for w in h:
+			print("----size=" + str(len(w)))
+			#print(w)
+			
+
+def random_hidden(hid, szo, szw):
+
+	"""
+	print("nb_hidden:" + str(nb_hidden))
+	for nb in range(nb_hidden):#2
+		for nh in range(len(sz_hidden)):
+			print("sz_hidden:" + str(sz_hidden[nh]))
+			for n in range(sz_hidden[nh]):#16
+				print(n)
+				w = []
+				print("size[n]:" + str(size[n]))
+				for i in range(size[n]):
+					w.append(random.random())
+
+				#hid.append(w)
+	"""
+
+	
+	for no in range(szo):
+		wl = []
+		for nw in range(szw):
+			wl.append(random.random())
+
+		hid.append(wl)
+
+
+	
+
+def random_hidden_bias_RELU(hid, size):
+
+	for n in range(nb_hidden):
+		
+		w = []
+		for i in range(size[n]):
+			ns = random.random()
+			nb = random.random()*100
+			if ns > 5:
+				w.append(nb)
+			else:
+				w.append(-nb)
+		hid.append(w)
+
+def random_hidden_bias_sigmoid(hid, size):
+
+	for n in range(nb_hidden):
+		
+		w = []
+		for i in range(size[n]):
+			w.append(random.random())
+
+		hid.append(w)
+
+def random_output_w(hid, szo, szw):
+		
+	"""w = []
+	for i in range(size):
+		w.append(random.random())
+
+	hid.append(w)"""
+	
+	for no in range(szo):
+		wl = []
+		for nw in range(szw):
+			wl.append(random.random())
+
+		hid.append(wl)
+
+def random_output_bias_sigmoid(hid, size):
+
+	
+	w = []
+	for i in range(size):
+		w.append(random.random()*100)
+
+	hid.append(w)
+
+def display_hiddenw():
+	print("hidden weight")
+	display_hidden_w(hiddenw)
+
+def random_weight():
+	hiddenw.append([])	
+	#out in
+	random_hidden(hiddenw[0], size_hidden, size_input)
+	hiddenw.append([])
+	random_hidden(hiddenw[1], size_hidden2, size_hidden)
+
+def display_hiddenb():
+	print("hidden bias")
+	display_hidden(hiddenb)
+
+def random_bias(type):
+	if type == "RELU":
+		random_hidden_bias_RELU(hiddenb, size_hidden_bias)
+	elif type == "sigmoid":
+		random_hidden_bias_sigmoid(hiddenb, size_hidden_bias)
+
+def display_hiddeneuron():
+	print("hidden neuron")
+	display_hidden(hidden)
+
+def random_outputw():
+	outputw.append([])
+	random_output_w(outputw[0], 2, size_hidden2)
+
+def display_outputw():
+	for h in outputw:
+		print("size=" + str(len(h)))
+		for w in h:
+			print("----size=" + str(len(w)))
+			print(w)
+
+def random_outputb():
+	random_output_bias_sigmoid(outputb, size_output) 
+
+def display_outputb():
+	print("output bias")
+	display_hidden(outputb)
+
+#***********************************************************************************
+
+def sigmoid(x):
+	#try:
+		
+	return 1 / (1 + math.exp(-x))
+	"""except OverflowError:
+		print("x:" + str(x))
+		#print("exp:" + str(math.exp(-x)))
+		return 1 / (1 + math.exp(0))
+	"""
+def RELU(x):
+	if x <= 0:
+		return 0
+	else:
+		return x
+
+
+#***********************************************************************************
+
+def calc_output_sigmoid(inp, hw, hb, size_in, size_out):
+	out = []
+	
+	for o in range(size_out):
+ 		ri = 0
+		for i in range(size_in):
+			ri += inp[i] * hw[i]
+			
+		out.append( sigmoid( ri + hb[o] ) )
+
+
+	return out
+
+def calc_output_RELU(inp, hw, hb, size_in, size_out):
+	out = []
+	
+	for o in range(size_out):
+ 		ri = 0
+		for i in range(size_in):
+			ri += inp[i] * hw[i]
+			
+		out.append( RELU( ri + hb[o] ) )
+
+
+	return out
+
+def calc_output_sigmoid2(inp, hw, hb, size_in, size_out):
+	out = []
+	for o in range(size_out):
+		ri = 0
+		for i in range(size_in):
+			ri += inp[i] * hw[o][i]
+
+		out.append( sigmoid( ri + hb[o] ) )
+
+	return out
+
+# [ [16[25]], [16[16]] ]
+def calc_output_RELU2(inp, hw, hb, size_in, size_out):
+	out = []
+	for o in range(size_out):
+		ri = 0
+		for i in range(size_in):
+			ri += inp[i] * hw[o][i]
+
+		out.append( RELU( ri + hb[o] ) )
+
+	return out
+
+#***********************************************************************************
+#backpropagation
+
+
+def Error(target, out):
+	return ((target * out) * (target * out)) * 0.5
+
+"""
+dEtot/dw5 = dEtot/dout1 * dout1/dnet1 * dnet1/dw5
+
+
+dEtot/dout1 = -(target1 - out1)
+dout1/dnet1 = dout1 x (1 - dout1)
+dnet1/dw5 = dout1
+
+w5' = w5 - lr x dEtot/dw5
+
+dE1/douth1 = dE1/dnet1 * dnet1/douth1
+		0.1384 * 0.4(w5)
+
+"""
+
+def derive(x):
+	return x * (1 - x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nna.py
@@ -0,0 +1,197 @@
+from nn import *
+from im import *
+
+
+
+#display_TEST()
+
+random_weight()
+display_hiddenw()
+
+
+
+random_bias("sigmoid")
+display_hiddenb()
+
+
+random_outputw()
+display_outputw()
+
+
+random_outputb()
+display_outputb()
+
+
+I = 0
+for X in range(10000):
+
+	
+
+	#del inputn[:]
+	inputn = []
+	inputn = test2inputn(TEST[I])
+	#print("l:"+str(len(inputn)))
+	#display_inputn(inputn)
+	#display_test(inputn, TEST_out[I])
+		
+	hidden = []
+	h = []
+	h = calc_output_sigmoid2(inputn, hiddenw[0], hiddenb[0], size_input, size_hidden)
+	#print("output in hid1")
+	#print(h)
+	hidden.append(h);
+
+	h = []
+	h = calc_output_sigmoid2(hidden[0], hiddenw[1], hiddenb[1], size_hidden, size_hidden2)
+	#print("output hid1 hid2")
+	#print(h)
+	hidden.append(h);
+
+	output = []
+	h = []
+	h = calc_output_sigmoid2(hidden[1], outputw[0], outputb[0], size_output_weight, size_output_bias)
+	print("output hid2 out")
+	print(h)
+	output.append(h);
+
+	name = ""
+	if (TEST_out[I]-1) == 0:
+		name = "croix"
+	else:
+		name="trait"
+
+	print(str(X) + " - " + str(I) + "=test_out:" + name)
+	if output[0][0] > output[0][1]:
+		print("C'est une croix")
+	else:
+		print("C'est un trai")
+
+	#----------------------------------------------------------------------
+	Loss = []
+
+	"""
+	dEtot/dw5 = dEtot/dout1 * dout1/dnet1 * dnet1/dw5
+
+
+	dEtot/dout1 = -(target1 - out1)
+	dout1/dnet1 = dout1 x (1 - dout1)
+	dnet1/dw5 = dout1
+
+	w5' = w5 - lr x dEtot/dw5
+
+	dE1/douth1 = dE1/dnet1 * dnet1/douth1
+			0.1384 * 0.4(w5)
+
+	"""
+	
+	for o in range(size_output):
+		if o == (TEST_out[I]-1):
+			Loss.append( (1 - output[0][o]) )#*(1 - output[0][o])) )
+		else:
+			Loss.append( -output[0][o] )#* (0-output[0][o])))
+	print("Loss=" + str(Loss))
+	#print("Test OUT:" + str(TEST_out))
+	
+	#Hid2 Out
+	for o in range(size_output):
+		d = derive(output[0][o])
+		db = LR * Loss[o] * d
+		outputb[0][o] = db
+		for i in range(size_hidden2):
+			outputw[0][o][i] = db * hidden[1][i]
+
+
+	#Hid1 Hid2
+	Loss2 = []
+	err = (Loss[0]+Loss[1])*0.5
+	for o in range(size_hidden2):
+		Loss2.append(err)# * hidden[1][o])#Loss[0] +  hidden[1][o] * Loss[1])
+		
+	#print("Loss2=" + str(Loss2))
+	
+
+	#Hid1 Hid2
+	for o in range(size_hidden2):
+		d = derive(hidden[1][o])
+		db = LR * Loss2[o] * d
+		hiddenb[1][o] = db
+		for i in range(size_hidden):
+			hiddenw[1][o][i] = db * hidden[0][i]
+
+
+	#In Hid1
+	Loss3 = []
+	err = 0
+	for e in range(size_hidden2):
+		err += Loss2[e]
+	err = err / 16
+	for o in range(size_hidden):
+		Loss3.append( err )#* hidden[0][o] )#Loss2[i] )
+		
+	#print("Loss3=" + str(Loss3))
+
+
+	#In Hid1
+	for o in range(size_hidden):
+		d = derive(hidden[0][o])
+		db = LR * Loss3[o] * d
+		hiddenb[0][o] = db
+		for i in range(size_input):
+			hiddenw[0][o][i] = db * inputn[i]
+
+	I+=1
+	if I == len(TEST):
+		I = 0
+
+
+#-------------------------------test -------------------------------------------
+"""
+print("--------------------------TEST-----------------------------")
+print("--------------------------TEST-----------------------------")
+print("--------------------------TEST-----------------------------")
+print("--------------------------TEST-----------------------------")
+print("--------------------------TEST-----------------------------")
+print("--------------------------TEST-----------------------------")
+
+for X in range(len(TEST)):
+
+	
+
+	#del inputn[:]
+	inputn = []
+	inputn = test2inputn(TEST[X])
+	print("l:"+str(len(inputn)))
+	display_inputn(inputn)
+	display_test(inputn, TEST_out[X])
+		
+	hidden = []
+	h = []
+	h = calc_output_sigmoid2(inputn, hiddenw[0], hiddenb[0], size_input, size_hidden)
+	#print("output in hid1")
+	#print(h)
+	hidden.append(h);
+
+	h = []
+	h = calc_output_sigmoid2(hidden[0], hiddenw[1], hiddenb[1], size_hidden, size_hidden)
+	#print("output hid1 hid2")
+	#print(h)
+	hidden.append(h);
+
+	output = []
+	h = []
+	h = calc_output_sigmoid2(hidden[1], outputw[0], outputb[0], size_output_weight, size_output_bias)
+	print("output hid2 out")
+	print(h)
+	output.append(h);
+
+	if output[0][0] > output[0][1]:
+		print("C'est une croix")
+	else:
+		print("C'est un trai")
+
+
+
+"""
+
+
+
