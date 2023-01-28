aaf9e8969416c37531881d3efc52d28589fa1990
Régis Meyssonnier <67794100+regismeyssonnier@users.noreply.github.com>
2023-01-28 22:01:47 +0100

    Add files via upload

--- NeuralNetworks.cpp
+++ NeuralNetworks.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 using namespace std;
 
 
@@ -17,7 +18,7 @@ struct NN1 {
     vector<double> inputb, hiddenb, biasb, hiddenob, biasob;
     vector<vector<double>>weightb, weightob;
     int SZI, SZH, SZO, SZW, SZWO;
-    double LR = 0.0009, beta = 0.9, LRB = 0.00009, LRT = 0.00000001;
+    double LR = 0.0009, beta = 0.9, LRB = 0.000009, LRT = 0.00000001;
     NN1(int inp, int hl, int out) {
         SZI = inp;
         SZH = hl;
@@ -49,12 +50,92 @@ struct NN1 {
                 Ewo[i].push_back(0);
             }
         }
-        biaso.push_back((((double)(rand() % 40) + 1.0) - 0.0) / 100.0 );
+
+        for(int i = 0;i < SZO;++i)
+            biaso.push_back((((double)(rand() % 40) + 1.0) - 0.0) / 100.0 );
 
         for (int i = 0; i < SZH; ++i) {
             bias.push_back((((double)(rand() % 40) + 1.0) - 0.0) / 100.0 );
             //biaso[i] = ((rand() % 4) + 1) - 2;
         }
+
+    }
+
+    void Init_Training(
+        vector<double> _hidden,
+        vector<double> _bias,
+        vector<double> _hiddeno,
+        vector<double> _biaso,
+        vector<vector<double>> _weight,
+        vector<vector<double>> _weighto) {
+
+        hidden.swap(_hidden);
+        bias.swap(_bias);
+        hiddeno.swap(_hiddeno);
+        biaso.swap(_biaso);
+        weight.swap(_weight);
+        weighto.swap(_weighto);
+    }
+
+    void dump_data_cpp() {
+        cout << "/* hidden */" << endl;
+        cout << '{';
+        for (int i = 0; i < hidden.size(); ++i) {
+            cout << hidden[i];
+            if (i < hidden.size() - 1)cout << ", ";
+        }
+        cout << "}, \n";
+
+        cout << "/* bias */" << endl;
+        cout << '{';
+        for (int i = 0; i < bias.size(); ++i) {
+            cout << bias[i];
+            if (i < bias.size() - 1)cout << ", ";
+        }
+        cout << "}, \n";
+
+        cout << "/* hiddeno */" << endl;
+        cout << '{';
+        for (int i = 0; i < hiddeno.size(); ++i) {
+            cout << hiddeno[i];
+            if (i < hiddeno.size() - 1)cout << ", ";
+        }
+        cout << "}, \n";
+
+        cout << "/* biaso */" << endl;
+        cout << '{';
+        for (int i = 0; i < biaso.size(); ++i) {
+            cout << biaso[i];
+            if (i < biaso.size() - 1)cout << ", ";
+        }
+        cout << "}, \n";
+
+        cout << "/* weight */" << endl;
+        cout << '{';
+        for (int i = 0; i < weight.size(); ++i) {
+            cout << '{';
+            for (int j = 0; j < weight[i].size(); ++j) {
+                cout << weight[i][j];
+                if (j < weight[i].size() - 1)cout << ", ";
+            }
+            cout << '}';
+            if (i < weight.size() - 1)cout << ",\n";
+        }
+        cout << "}, \n";
+
+        cout << "/* weihgto */" << endl;
+        cout << '{';
+        for (int i = 0; i < weighto.size(); ++i) {
+            cout << '{';
+            for (int j = 0; j < weighto[i].size(); ++j) {
+                cout << weighto[i][j];
+                if (j < weighto[i].size() - 1)cout << ", ";
+            }
+            cout << '}';
+            if (i < weighto.size() - 1)cout << ",\n";
+        }
+        cout << "} \n";
+
 
     }
 
@@ -193,7 +274,7 @@ struct NN1 {
         }
 
         double db = LRB * cost;
-        Ebo[0] = beta * Ebo[0] + (1 - beta) * db;
+        Ebo[0] = beta * Ebo[0] + (1 - beta) * db * db;
         biaso[0] -= Ebo[0];
         //cout << "bug b2 " << endl;
 
@@ -218,7 +299,7 @@ struct NN1 {
 
                 for (int i = 0; i < SZH; ++i) {
                     double db = LRB * cost * weighto[jo][io] * dRelu(hidden[i]);
-                    Eb[i] = beta * Eb[i] + (1 - beta) * db;
+                    Eb[i] = beta * Eb[i] + (1 - beta) * db * db;
                     bias[i] -= Eb[i];
                 }
 
@@ -247,10 +328,367 @@ struct NN1 {
             cout << bias[i] << endl;
         }
 
-        cout << "biaso" << endl << biaso[0] << endl;
+
+        cout << "biaso" << endl;
+        for (int i = 0; i < SZO; ++i) {
+            cout << biaso[i] << endl;
+        }
     }
 
     void Train(vector<double> input, int target, int nb) {
+        build(input);
+        for (int i = 0; i < nb; ++i) {
+            Compute();
+            Backpropagation1(target);
+
+        }
+
+    }
+
+    void TrainRMSprop(vector<double> input, int target, int nb) {
+        build(input);
+        for (int i = 0; i < nb; ++i) {
+            Compute();
+            BackpropagationRMSprop(target);
+
+        }
+
+    }
+
+
+};
+
+struct NN2 {
+
+    vector<double> input, hidden, bias, hiddeno, biaso, Eb, Ebo;
+    vector<vector<double>>weight, weighto, Ew, Ewo;
+    vector<double> inputb, hiddenb, biasb, hiddenob, biasob;
+    vector<vector<double>>weightb, weightob;
+    int SZI, SZH, SZO, SZW, SZWO;
+    double LR = 0.000009, beta = 0.9, LRB = 0.000009, LRT = 0.00000001;
+    NN2(int inp, int hl, int out) {
+        SZI = inp;
+        SZH = hl;
+        SZW = SZI * SZH;
+        SZWO = out * hl;
+        SZO = out;
+
+        hidden.resize(SZH);
+        hiddeno.resize(SZO);
+        Eb.resize(SZH, 0);
+        Ebo.resize(SZO, 0);
+
+        srand(time(NULL));
+
+        for (int i = 0; i < SZI; ++i) {
+            weight.push_back({});
+            Ew.push_back({});
+            for (int j = 0; j < SZH; ++j) {
+                weight[i].push_back((((double)(rand() % 40) + 1.0) - 0.0) / 100.0);
+                Ew[i].push_back(0);
+            }
+        }
+
+        for (int i = 0; i < SZH; ++i) {
+            weighto.push_back({});
+            Ewo.push_back({});
+            for (int j = 0; j < SZO; ++j) {
+                weighto[i].push_back((((double)(rand() % 40) + 1.0) - 0.0) / 100.0);
+                Ewo[i].push_back(0);
+            }
+        }
+
+        for (int i = 0; i < SZO; ++i)
+            biaso.push_back((((double)(rand() % 40) + 1.0) - 0.0) / 100.0);
+               
+        for (int i = 0; i < SZH; ++i) {
+            bias.push_back((((double)(rand() % 40) + 1.0) - 0.0) / 100.0);
+            //biaso[i] = ((rand() % 4) + 1) - 2;
+        }
+
+    }
+
+    void Init_Training(
+        vector<double> _hidden,
+        vector<double> _bias,
+        vector<double> _hiddeno,
+        vector<double> _biaso,
+        vector<vector<double>> _weight,
+        vector<vector<double>> _weighto) {
+
+        hidden.swap(_hidden);
+        bias.swap(_bias);
+        hiddeno.swap(_hiddeno);
+        biaso.swap(_biaso);
+        weight.swap(_weight);
+        weighto.swap(_weighto);
+    }
+
+    void dump_data_cpp() {
+        cout << "/* hidden */" << endl;
+        cout << '{';
+        for (int i = 0; i < hidden.size(); ++i) {
+            cout << hidden[i];
+            if (i < hidden.size() - 1)cout << ", ";
+        }
+        cout << "}, \n";
+
+        cout << "/* bias */" << endl;
+        cout << '{';
+        for (int i = 0; i < bias.size(); ++i) {
+            cout << bias[i];
+            if (i < bias.size() - 1)cout << ", ";
+        }
+        cout << "}, \n";
+
+        cout << "/* hiddeno */" << endl;
+        cout << '{';
+        for (int i = 0; i < hiddeno.size(); ++i) {
+            cout << hiddeno[i];
+            if (i < hiddeno.size() - 1)cout << ", ";
+        }
+        cout << "}, \n";
+
+        cout << "/* biaso */" << endl;
+        cout << '{';
+        for (int i = 0; i < biaso.size(); ++i) {
+            cout << biaso[i];
+            if (i < biaso.size() - 1)cout << ", ";
+        }
+        cout << "}, \n";
+
+        cout << "/* weight */" << endl;
+        cout << '{';
+        for (int i = 0; i < weight.size(); ++i) {
+            cout << '{';
+            for (int j = 0; j < weight[i].size(); ++j) {
+                cout << weight[i][j];
+                if (j < weight[i].size() - 1)cout << ", ";
+            }
+            cout << '}';
+            if (i < weight.size() - 1)cout << ",\n";
+        }
+        cout << "}, \n";
+
+        cout << "/* weihgto */" << endl;
+        cout << '{';
+        for (int i = 0; i < weighto.size(); ++i) {
+            cout << '{';
+            for (int j = 0; j < weighto[i].size(); ++j) {
+                cout << weighto[i][j];
+                if (j < weighto[i].size() - 1)cout << ", ";
+            }
+            cout << '}';
+            if (i < weighto.size() - 1)cout << ",\n";
+        }
+        cout << "} \n";
+
+
+    }
+
+    void build(vector<double> inp) {
+        input.swap(inp);
+    }
+
+    void Copy(vector<double> s, vector<double> d) {
+        d.resize(s.size());
+        for (int i = 0; i < s.size(); ++i) {
+            d[i] = s[i];
+        }
+    }
+
+    void Copy2(vector<vector<double>> s, vector<vector<double>> d) {
+        d.resize(s.size());
+        for (int i = 0; i < s.size(); ++i) {
+            d[i].resize(s[i].size());
+            for (int j = 0; j < s[i].size(); ++i) {
+                d[i][j] = s[i][j];
+            }
+
+        }
+    }
+
+    void Swap() {
+        //Copy(input, inputb);
+        Copy(hidden, hiddenb);
+        Copy(hiddeno, hiddenob);
+        Copy2(weight, weightb);
+        Copy2(weighto, weightob);
+        Copy(bias, biasb);
+        Copy(biaso, biasob);
+    }
+
+    void Swap2() {
+        //Copy(inputb, input);
+        Copy(hiddenb, hidden);
+        Copy(hiddenob, hiddeno);
+        Copy2(weightb, weight);
+        Copy2(weightob, weighto);
+        Copy(biasb, bias);
+        Copy(biasob, biaso);
+    }
+
+    void Compute() {
+        // cout << "bug 1 " << endl;
+         //hidden
+        for (int i = 0; i < SZH; ++i) {
+            int h = 0;
+            for (int j = 0; j < SZI; ++j) {
+                h += input[j] * weight[j][i];
+            }
+            h += bias[i];
+            if (h < 0)h = 0;
+            hidden[i] = h;
+
+        }
+        //cout << "bug 2 " << endl;
+
+        //out
+        for (int i = 0; i < SZO; ++i) {
+            int h = 0;
+            for (int j = 0; j < SZH; ++j) {
+                h += hidden[j] * weighto[j][i];
+            }
+            h += biaso[0];
+            if (h < 0)h = 0;
+            hiddeno[i] = h;
+
+        }
+
+        //cout << "bug 3 " << endl;
+    }
+
+    double dRelu(double v) {
+        //return 1.0;
+        if (v > 0.0)return 1.0;
+        else return 0.0;
+    }
+
+
+    void Backpropagation1(vector<double> target) {
+
+        vector<double> cost(SZO);
+        for (int i = 0; i < cost.size(); ++i) {
+            cost[i] = 2 * (hiddeno[i] - target[i]);
+
+        }
+
+
+        //cout << "bug b1 " << endl;
+        for (int j = 0; j < SZH; ++j) {
+            for (int i = 0; i < SZO; ++i) {
+                weighto[j][i] -= LRT * cost[i] * hidden[j];
+            }
+        }
+
+        for (int i = 0; i < SZO; ++i) {
+            biaso[i] -= LRT * cost[i];
+        }
+        //cout << "bug b2 " << endl;
+
+        for (int j = 0; j < SZI; ++j) {
+            for (int i = 0; i < SZH; ++i) {
+                for (int jo = 0; jo < SZH; ++jo) {
+                    for (int io = 0; io < SZO; ++io) {
+                        weight[j][i] -= LRT * cost[io] * weighto[jo][io] * dRelu(hidden[i]) * input[j];
+                    }
+                }
+            }
+        }
+        //cout << "bug b3 " << endl;
+        for (int jo = 0; jo < SZH; ++jo) {
+            for (int io = 0; io < SZO; ++io) {
+
+                for (int i = 0; i < SZH; ++i) {
+                    bias[i] -= LRT * cost[io] * weighto[jo][io] * dRelu(hidden[i]);
+                }
+
+            }
+        }
+        //cout << "bug b4 " << endl;
+
+
+    }
+
+    void BackpropagationRMSprop(int target) {
+
+        int cost = 2 * (hiddeno[0] - target);
+        //cout << "bug b1 " << endl;
+        for (int j = 0; j < SZH; ++j) {
+
+            for (int i = 0; i < SZO; ++i) {
+                double dw = LR * cost * hidden[j];
+                Ewo[j][i] = beta * Ewo[j][i] + (1 - beta) * dw * dw;
+                //cout << "Ewo :" << Ewo[j][i] << " " << (LR / sqrt(Ewo[j][i])) * dw;
+                double sqe = sqrt(Ewo[j][i]), lre;
+                if (sqe == 0)lre = 0;
+                else lre = LR / sqe;
+                weighto[j][i] -= lre * dw;
+            }//cout << endl;
+
+        }
+
+        double db = LRB * cost;
+        Ebo[0] = beta * Ebo[0] + (1 - beta) * db * db;
+        biaso[0] -= Ebo[0];
+        //cout << "bug b2 " << endl;
+
+        for (int j = 0; j < SZI; ++j) {
+            for (int i = 0; i < SZH; ++i) {
+                for (int jo = 0; jo < SZH; ++jo) {
+                    for (int io = 0; io < SZO; ++io) {
+                        double dw = LR * cost * weighto[jo][io] * dRelu(hidden[i]) * input[j];
+                        Ew[j][i] = beta * Ew[j][i] + (1 - beta) * dw * dw;
+                        //cout << "Ew :" << Ew[j][i] << " " << (LR / sqrt(Ew[j][i])) * dw;
+                        double sqe = sqrt(Ew[j][i]), lre;
+                        if (sqe == 0)lre = 0;
+                        else lre = LR / sqe;
+                        weight[j][i] -= lre * dw;
+                    }
+                }
+            }
+        }
+        //cout << "bug b3 " << endl;
+        for (int jo = 0; jo < SZH; ++jo) {
+            for (int io = 0; io < SZO; ++io) {
+
+                for (int i = 0; i < SZH; ++i) {
+                    double db = LRB * cost * weighto[jo][io] * dRelu(hidden[i]);
+                    Eb[i] = beta * Eb[i] + (1 - beta) * db * db;
+                    bias[i] -= Eb[i];
+                }
+
+            }
+        }
+        //cout << "bug b4 " << endl;
+
+
+    }
+
+    double get_result(int i) {
+        return hiddeno[i];
+    }
+
+    void dump() {
+        cout << "weight" << endl;
+        for (int i = 0; i < SZI; ++i) {
+            cout << i << ": ";
+            for (int j = 0; j < SZH; ++j) {
+                cout << weight[i][j] << " ";
+            }cout << endl;
+        }
+
+        cout << "biash" << endl;
+        for (int i = 0; i < SZH; ++i) {
+            cout << bias[i] << endl;
+        }
+
+        cout << "biaso" << endl;
+        for (int i = 0; i < SZO; ++i)
+            cout << biaso[i] << endl;
+    }
+
+    void Train(vector<double> input, vector<double> target, int nb) {
         build(input);
         for (int i = 0; i < nb; ++i) {
             Compute();
@@ -279,8 +717,8 @@ int main()
     std::cout << "Hello World!\n";
     vector<double> a = { 1, 1 };
    
-
-    NN1 nn(3, 4, 1);
+    NN2 nn2(4, 4, 2);
+    //NN1 nn(4, 16, 1);
     /*nn.dump();
     nn.TrainRMSprop({1000, 0}, 1000, 100);
     nn.dump();
@@ -367,30 +805,64 @@ int main()
     nn.TrainRMSprop({ 5000, -25, -25 }, 200, 10);
     nn.dump();*/
 
-    nn.dump();
+    /*nn.dump();
 
-    double d[] = { 1000, 5000 };
-    double v[] = { 50, 120 };
+    double d[] = {1000, 5000};
+    double v[] = { 50, 200 };
 
     for (int s = 0; s < 2; ++s) {
-        for (double i = -180; i <= 180; i += 20) {
-            for (double j = -180; j <= 180; j += 20) {
-                nn.TrainRMSprop({ d[s], i, j }, v[s], 10);
+        for (int t = 0; t < 2; ++t) {
+            for (double i = -180; i <= 180; i += 20) {
+                for (double j = -180; j <= 180; j += 20) {
+                    double mi = min(d[t], d[s]);
+                    nn.TrainRMSprop({ d[s], d[t], i, j }, (mi == 1000)?v[0]:v[1], 10);
+                }
             }
         }
     }
 
-    nn.dump();
+    
+    cout << "----------------------------------" << endl << endl;
+    nn.dump_data_cpp();*/
 
-    nn.build({ 2000, 120, 25 });//240
+
+    /*nn.build({500, 5000, 120, 25});//240
     nn.Compute();
     cout << "res 1 " << nn.get_result() << endl;
 
-    nn.build({ 5000, 60, 60});//834
+    nn.build({ 5000, 5000, 60, 60});//834
     nn.Compute();
     cout << "res 2 " << nn.get_result() << endl;
     
-    nn.build({ 7563, 88, 46 });//303
+    nn.build({ 5000, 100, 120, 25 });//303
     nn.Compute();
     cout << "res 3 " << nn.get_result() << endl;
+
+    nn.build({ 1000, 4914, 64, 75 });//303
+    nn.Compute();
+    cout << "res 4 " << nn.get_result() << endl*/
+
+    nn2.dump();
+
+    double d[] = { 1000, 5000 };
+    double v[] = { 100, 200 };
+
+    for (int s = 0; s < 2; ++s) {
+        for (int t = 0; t < 2; ++t) {
+            for (double i = -180; i <= 180; i += 20) {
+                for (double j = -180; j <= 180; j += 20) {
+                    double mi = min(d[t], d[s]);
+                    nn2.Train({ d[s], d[t], i, j }, {v[1] ,  v[0]  }, 10);
+                }
+            }
+        }
+    }
+
+    nn2.dump();
+    nn2.dump_data_cpp();
+
+    nn2.build({ 500, 5000, 160, 25 });//240
+    nn2.Compute();
+    cout << "res 1 " << nn2.get_result(0) << " " << nn2.get_result(1) << endl;
+
 }
